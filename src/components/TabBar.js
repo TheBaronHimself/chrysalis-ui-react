042b8e1374507f37944ad251ebf5b9463e51a212
TheBaronHimself <accounts@simonclaudius.com>
2018-09-25 21:35:43 +0200

    Refactor tab bar markup and stylings

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -5,29 +5,17 @@ export class TabBar extends Component {
     return (
       <div className="tabBar">
         <div className="logo">
-          <img className="symbol" src="/img/dygma-logo.svg" alt="" />
+          <img src="/img/dygma-logo.svg" alt="" />
         </div>
 
-        <div className="item selected" onclick="openTab(event, 'keys')">
-          <img
-            className="symbol"
-            src="/img/ic_keyboard_white_24px.svg"
-            alt=""
-          />
+        <div className="item selected">
+          <img src="/img/ic_keyboard_white_24px.svg" alt="" />
         </div>
-        <div className="item" onclick="openTab(event, 'leds')">
-          <img
-            className="symbol"
-            src="/img/ic_color_lens_white_24px.svg"
-            alt=""
-          />
+        <div className="item">
+          <img src="/img/ic_color_lens_white_24px.svg" alt="" />
         </div>
-        <div className="item" onclick="openTab(event, 'settings')">
-          <img
-            className="symbol"
-            src="/img/ic_settings_white_24px.svg"
-            alt=""
-          />
+        <div className="item">
+          <img src="/img/ic_settings_white_24px.svg" alt="" />
         </div>
       </div>
     );
