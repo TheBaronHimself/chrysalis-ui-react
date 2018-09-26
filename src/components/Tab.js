688ce3b0de3c9ac47e07c2674b2fcd433c1fec33
TheBaronHimself <accounts@simonclaudius.com>
2018-09-26 05:22:56 +0200

    Start of tab stylings

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -3,8 +3,19 @@ import React, { Component } from "react";
 export class Tab extends Component {
   render() {
     return (
-      <div>
-        <h1>Title</h1>
+      <div className="tab">
+        <div className="top-bar">
+          <div className="title">
+            <h1>Keymap Editor</h1>
+            <p>Configure your keymap</p>
+          </div>
+          <div className="top-controls">
+            <h1>Top Controls</h1>
+          </div>
+        </div>
+        <div className="tab-contents">
+          <h1>Tab Contents</h1>
+        </div>
       </div>
     );
   }
